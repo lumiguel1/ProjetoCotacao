f64fc9eca6ed491cd8e7b0979610ef1465d0d9a3
LUIZ MIGUEL REIS DE JESUS <luizmiguelreis2000@gmail.com>
2020-08-20 17:49:51 -0300

    Projeto Cotacao LADO - 1

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
-function clicar(){
-    alert("Clicou")
+function clicar1(){
+    var item = '';
+    item = document.getElementById('1');
+    src = item.getAttribute('src')
+    document.getElementById('tes').innerHTML = 
+    `<img src="${src}" alt="BitCoin">
+     <h1>Teste</h1>`;
+}
+function clicar2(){
+    var item = '';
+    item = document.getElementById('2');
+    src = item.getAttribute('src')
+    document.getElementById('tes').innerHTML = `<img src="${src}" alt="Dollar American">`;
+}
+function clicar3(){
+    var item = '';
+    item = document.getElementById('3');
+    src = item.getAttribute('src')
+    document.getElementById('tes').innerHTML = `<img src="${src}" alt="Dollar Canadian">`;
+}
+function clicar4(){
+    var item = '';
+    item = document.getElementById('4');
+    src = item.getAttribute('src')
+    document.getElementById('tes').innerHTML = `<img src="${src}" alt="Libra">`;
+}
+function clicar5(){
+    var item = '';
+    item = document.getElementById('5');
+    src = item.getAttribute('src')
+    document.getElementById('tes').innerHTML = `<img src="${src}" alt="Euro">`;
+}
+function clicar6(){
+    var item = '';
+    item = document.getElementById('6');
+    src = item.getAttribute('src')
+    document.getElementById('tes').innerHTML = `<img src="${src}" alt="Swiss Franco">`;
+}
+function clicar7(){
+    var item = '';
+    item = document.getElementById('7');
+    src = item.getAttribute('src')
+    document.getElementById('tes').innerHTML = `<img src="${src}" alt="Yen">`;
+}
+function clicar8(){
+    var item = '';
+    item = document.getElementById('8');
+    src = item.getAttribute('src')
+    document.getElementById('tes').innerHTML = `<img src="${src}" alt="Yuan">`;
 }
